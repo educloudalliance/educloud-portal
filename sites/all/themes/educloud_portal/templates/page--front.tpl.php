9eaea543e07acb54000c9a696e6357f4cf371fc7
Janne Hakala <janne.hakala@mediamaisteri.com>
2014-10-01 15:16:56 +0300

    Add frontpage styles.

--- sites/all/themes/educloud_portal/templates/page--front.tpl.php
+++ sites/all/themes/educloud_portal/templates/page--front.tpl.php
@@ -1,11 +1,14 @@
 <?php
 /**
  * @file
- * Returns the HTML for a single Drupal page.
+ * Returns the HTML for front page.
  *
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728148
  */
+
+unset($page['content']['system_main']['default_message']);
+
 ?>
 <div id="top-bar"></div>
 <div id="page">
@@ -83,19 +86,8 @@
       <?php print render($page['highlighted']); ?>
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
       <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
       <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
     </div>
 
   </div>
